0e2fe04a1439dbf900f3fb9b325381863376d43e
Tyr <langtuananh2424@gmail.com>
2024-12-27 20:28:23 +0700

    Setup database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\NhanVienController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('nhanviens', NhanVienController::class);
+Route::resource('hdlds', 'HDLDController');
+Route::resource('phongbans', 'PhongBanController');
+Route::resource('chucvus', 'ChucVuController');
+Route::resource('trinhdohocvans', 'TrinhDoHocVanController');
+Route::resource('luongs', 'LuongController');
